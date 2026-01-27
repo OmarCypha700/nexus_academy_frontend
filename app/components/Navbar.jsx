d2b03a81fa67346bd9b9480d4b23e9ab0a556fb3
Cypha <omarcypha@gmail.com>
2026-01-27 23:23:45 +0000

    rm prices

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -28,7 +28,7 @@ export default function Navbar() {
   const navLinks = [
     { name: "Dashboard", path: dashboardPath },
     { name: "Courses", path: "/courses" },
-    { name: "Pricing", path: "/prices" },
+    // { name: "Pricing", path: "/prices" },
   ];
 
   const isActive = (path) => pathname.startsWith(path);
